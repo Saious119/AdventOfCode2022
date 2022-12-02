70b26545098072d2a23e4bf6fd444dd3384a86a7
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2022-12-01 19:51:00 -0500

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day1
+    {
+        public void run()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            List<int> list = new List<int>();
+            int num = 0;
+            foreach(string line in input)
+            {
+                if(line != "")
+                {
+                    num += Int32.Parse(line);
+                }
+                if(line == "")
+                {
+                    list.Add(num);
+                    num = 0;
+                }
+            }
+            int max1 = list.Max();
+            list.Remove(max1);
+            int max2 = list.Max();
+            list.Remove(max2);
+            int max3 = list.Max();
+            int total = max1 + max2 + max3;
+            Console.WriteLine(total.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2022;
+
+Console.WriteLine("Hello, World!");
+Day1 day1 = new Day1();
+day1.run();
+Console.ReadLine();
