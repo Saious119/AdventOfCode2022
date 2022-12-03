592f7fd396d255e02ff8e45f7f6af7faec3239d7
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2022-12-03 00:27:50 -0500

    Day 3

---
+++ Day3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day3
+    {
+        public void Part1()
+        {
+            int sum = 0;
+            string[] input = File.ReadAllLines("input.txt");
+            foreach(string line in input)
+            {
+                string firstComp = line.Substring(0, line.Length / 2);
+                string secondComp = line.Substring(line.Length / 2);
+                foreach(char compType in firstComp)
+                {
+                    if (secondComp.Contains(compType))
+                    {
+                        sum += CalcPriority(compType);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        public void Part2()
+        {
+            int sum = 0;
+            string[] input = File.ReadAllLines("input.txt");
+            for(int i = 0; i < input.Length; i += 3)
+            {
+                foreach(char compType in input[i])
+                {
+                    if (input[i+1].Contains(compType) && input[i + 2].Contains(compType))
+                    {
+                        sum += CalcPriority(compType);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        private int CalcPriority(char compType)
+        {
+            if(compType > 'Z')
+            {
+                return compType - 96;
+            }
+            if(compType < 'a')
+            {
+                return compType - 38;
+            }
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 using AdventOfCode2022;
 
 Console.WriteLine("Hello, World!");
-Day2 day2 = new Day2();
-day2.Part2();
+Day3 day3 = new Day3();
+day3.Part2();
 Console.ReadLine();
