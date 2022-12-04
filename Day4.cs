6c91b5ae04e0d9fd992d2216dce34f7e05ff9e40
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2022-12-04 00:20:39 -0500

    Day 4

---
+++ Day4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day4
+    {
+        public void Part1()
+        {
+            int numOverlap = 0;
+            string[] input = File.ReadAllLines("input.txt");
+            foreach(string line in input)
+            {
+                string[] assignments = line.Split(',');
+                int bottonRange1 = Int32.Parse(assignments[0].Split('-')[0]);
+                int topRange1 = Int32.Parse(assignments[0].Split('-')[1]);
+                int bottonRange2 = Int32.Parse(assignments[1].Split('-')[0]);
+                int topRange2 = Int32.Parse(assignments[1].Split('-')[1]);
+                if(bottonRange1 <= bottonRange2 && topRange1 >= topRange2) { numOverlap++; }
+                else if (bottonRange2 <= bottonRange1 && topRange2 >= topRange1) { numOverlap++; }
+            }
+            Console.WriteLine("Overlaps = {0}", numOverlap);
+        }
+        public void Part2()
+        {
+            int numOverlap = 0;
+            string[] input = File.ReadAllLines("input.txt");
+            foreach (string line in input)
+            {
+                string[] assignments = line.Split(',');
+                int bottonRange1 = Int32.Parse(assignments[0].Split('-')[0]);
+                int topRange1 = Int32.Parse(assignments[0].Split('-')[1]);
+                int bottonRange2 = Int32.Parse(assignments[1].Split('-')[0]);
+                int topRange2 = Int32.Parse(assignments[1].Split('-')[1]);
+                if (bottonRange1 <= bottonRange2 && topRange1 >= topRange2) { numOverlap++; }
+                else if (bottonRange2 <= bottonRange1 && topRange2 >= topRange1) { numOverlap++; }
+                else if(bottonRange1 <= bottonRange2 && topRange1 >= bottonRange2) { numOverlap++; }
+                else if(bottonRange1 <= topRange2 && topRange1 >= topRange2) { numOverlap++; }
+            }
+            Console.WriteLine("Overlaps = {0}", numOverlap);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 using AdventOfCode2022;
 
 Console.WriteLine("Hello, World!");
-Day3 day3 = new Day3();
-day3.Part2();
+Day4 day4 = new Day4();
+day4.Part2();
 Console.ReadLine();
